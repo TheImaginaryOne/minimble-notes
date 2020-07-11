6edcf9d20935e0191ead021ac15e114171b6803b
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2020-07-11 21:52:00 +1200

    Add option for default directory, disable custom filenames

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,8 @@ struct EditNoteOptions {
 struct NewNoteOptions {
     #[clap(short, long)]
     name: String,
-    #[clap(short, long, parse(from_os_str))]
-    filename: Option<OsString>,
+    #[clap(short, long)]
+    filename: Option<String>,
     #[clap(short, long)]
     directory: Option<PathBuf>,
 }
@@ -43,10 +43,9 @@ fn main() {
     }
 }
 fn run(options: Options) -> anyhow::Result<()> {
-    let base_dirs = BaseDirs::new().context("No base directory found")?;
-    let data_dir = base_dirs.data_dir();
+    let notes_dir = std::env::var("NOTES_DIR").context("Notes directory not set")?;
 
-    let data_path = data_dir.join(Path::new("minimble_data.json"));
+    let data_path = Path::new(&notes_dir).join(Path::new("minimble_data.json"));
     let mut note_data = data::load_note_data(&data_path).with_context(|| {
         format!(
             "Failed to read note data from {}",
@@ -54,9 +53,9 @@ fn run(options: Options) -> anyhow::Result<()> {
         )
     })?;
     match options.subcommand {
-        SubCommand::Edit(o) => edit(o, &note_data)?,
+        SubCommand::Edit(o) => edit(o, Path::new(&notes_dir), &note_data)?,
         SubCommand::New(o) => {
-            new(o, &mut note_data)?;
+            new(o, Path::new(&notes_dir), &mut note_data)?;
             data::save_note_data(&note_data, &data_path).with_context(|| {
                 format!("Failed to write data to {}", data_path.to_string_lossy())
             })?;
@@ -64,7 +63,7 @@ fn run(options: Options) -> anyhow::Result<()> {
     }
     Ok(())
 }
-fn new(options: NewNoteOptions, note_data: &mut NoteData) -> anyhow::Result<()> {
+fn new(options: NewNoteOptions, notes_dir: &Path, note_data: &mut NoteData) -> anyhow::Result<()> {
     if note_data.has_note(&options.name) {
         return Err(anyhow::anyhow!(
             "Note named \"{}\" already exists",
@@ -72,12 +71,9 @@ fn new(options: NewNoteOptions, note_data: &mut NoteData) -> anyhow::Result<()> 
         ));
     }
     let editor = std::env::var("EDITOR").context("Editor not set")?;
-    let filename = options
-        .filename
-        .unwrap_or(push_os_string(OsString::from(options.name.clone()), ".md"));
+    let filename = options.name.clone() + ".md";
 
-    let note_path =
-        Path::new(&options.directory.expect("TODO add default dir")).join(Path::new(&filename));
+    let note_path = notes_dir.join(Path::new(&filename));
 
     let _ = Command::new(editor)
         .arg(note_path.clone())
@@ -90,7 +86,7 @@ fn new(options: NewNoteOptions, note_data: &mut NoteData) -> anyhow::Result<()> 
             note_path.to_string_lossy()
         )));
     }
-
+/*
     note_data.add_note(
         options.name,
         absolute_path(&note_path).with_context(|| {
@@ -99,15 +95,14 @@ fn new(options: NewNoteOptions, note_data: &mut NoteData) -> anyhow::Result<()> 
                 note_path.to_string_lossy()
             )
         })?,
-    );
+    );*/
     println!("Saved note");
     Ok(())
 }
-fn edit(options: EditNoteOptions, note_data: &NoteData) -> anyhow::Result<()> {
+fn edit(options: EditNoteOptions, notes_dir: &Path, note_data: &NoteData) -> anyhow::Result<()> {
     let editor = std::env::var("EDITOR").context("Editor not set")?;
 
-    let path = note_data.get_note(&options.name)
-        .context("Note with this name does not exist")?;
+    let path = notes_dir.join(Path::new(&(options.name + ".md")));
 
     let _ = Command::new(editor)
         .arg(path)
@@ -115,11 +110,6 @@ fn edit(options: EditNoteOptions, note_data: &NoteData) -> anyhow::Result<()> {
         .context("Failed to save note")?;
     println!("Saved note");
     Ok(())
-}
-
-fn push_os_string(mut s: OsString, ext: &str) -> OsString {
-    s.push(ext);
-    s
 }
 
 use path_clean::PathClean;
